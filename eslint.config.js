d0fafca30ffaa4623dd32f463eb6046f461801c1
Diep Thuy Han <diepthuyhan@diepthuyhan>
2025-08-04 18:38:02 +0700

    update

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const typescriptParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+    rules: {
+      ...typescriptEslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  {
+    ignores: [
+      'node_modules/**',
+      'cdk.out/**',
+      '*.js',
+      '*.d.ts',
+      'jest.config.js',
+      'eslint.config.js'
+    ],
+  },
+];
